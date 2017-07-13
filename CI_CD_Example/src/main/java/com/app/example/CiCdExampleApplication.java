ce2ed29d2b0237e689eedd001ee7825df73c54e6
Optimus-PC\Optimus <eh.yogendra@gmail.com>
2017-07-13 21:17:42 +0530

    no message

--- CI_CD_Example/src/main/java/com/app/example/CiCdExampleApplication.java
+++ CI_CD_Example/src/main/java/com/app/example/CiCdExampleApplication.java
@@ -1,17 +1,24 @@
 package com.app.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class CiCdExampleApplication {
 
 	@RequestMapping("/greet")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model){
         return "Hello "+name+"!";
+    }
+
+	@RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
     }
 
 	public static void main(String[] args) {
